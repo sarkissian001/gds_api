d8cafad1c8bde94f3734232ab9da82713cfb5ec7
arsar7 <sargsyan_arman@outlook.com>
2023-11-07 16:20:58 +0000

    updated code to wait for topic if not created

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,7 @@ WORKDIR $APP_HOME
 COPY build.gradle settings.gradle $APP_HOME
 COPY gradle $APP_HOME/gradle
 COPY --chown=gradle:gradle . .
-RUN gradle build || return 0
-COPY . .
-RUN gradle clean build
+RUN gradle clean build || return 0
 
 # Actual container
 FROM openjdk:17-jdk-slim
--- src/main/java/com/streamer/streamer_api/config/KafkaConfig.java
+++ src/main/java/com/streamer/streamer_api/config/KafkaConfig.java
@@ -1,20 +1,34 @@
 package com.streamer.streamer_api.config;
 
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.ListTopicsOptions;
+import org.apache.kafka.common.errors.TimeoutException;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Time;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Materialized;
+import org.slf4j.LoggerFactory;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
 import jakarta.annotation.PostConstruct;
 
 import java.util.Properties;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Configuration
 public class KafkaConfig {
@@ -29,7 +43,8 @@ public class KafkaConfig {
     private String kafkaApplicationId; 
 
     private String storeName = idTopic + "-store";
-    
+    private static final Logger logger = LoggerFactory.getLogger(KafkaConfig.class);
+
     // Validate env vars 
    @PostConstruct
     public void validate() {
@@ -77,9 +92,51 @@ public class KafkaConfig {
     }
 
     @Bean
-    public KafkaStreams kafkaStreams(StreamsBuilder streamsBuilder, Properties kStreamConfigs) {
+    public KafkaStreams kafkaStreams(StreamsBuilder streamsBuilder, Properties kStreamConfigs) throws InterruptedException {
+        // Wait for the topic to become available
+        waitForTopic(idTopic, kStreamConfigs);
+
+        logger.info("Topic '{}' is available. Creating KafkaStreams...", idTopic);
+
         KafkaStreams kafkaStreams = new KafkaStreams(streamsBuilder.build(), kStreamConfigs);
         kafkaStreams.start();
+
+        logger.info("KafkaStreams started successfully.");
         return kafkaStreams;
     }
+
+    private void waitForTopic(String topicName, Properties kStreamConfigs) throws InterruptedException {
+        boolean topicAvailable = false;
+        long startTime = System.currentTimeMillis();
+        long maxRetryTimeMillis = TimeUnit.MINUTES.toMillis(10);
+
+        while (!topicAvailable && System.currentTimeMillis() - startTime < maxRetryTimeMillis) {
+            try {
+                AdminClient adminClient = AdminClient.create(kStreamConfigs);
+                ListTopicsOptions listTopicsOptions = new ListTopicsOptions().timeoutMs(5000); // Set a timeout for listing topics
+                Set<String> topicNames = adminClient.listTopics(listTopicsOptions).names().get();
+                boolean topicExists = topicNames.contains(topicName);
+
+                adminClient.close();
+
+                if (topicExists) {
+                    topicAvailable = true;
+                    logger.info("Topic '{}' is now available.", topicName);
+                } else {
+                    logger.info("Topic '{}' not found. Waiting for 5 seconds before retrying...", topicName);
+                    Thread.sleep(5000); // Wait for 5 seconds before retrying
+                }
+            } catch (ExecutionException | TimeoutException e) {
+                // Handle exceptions
+                logger.error("Error checking topic '{}': {}", topicName, e.getMessage());
+                Thread.sleep(5000); // Wait for 5 seconds before retrying
+            }
+        }
+
+        if (!topicAvailable) {
+            logger.error("Topic '{}' not found after waiting for 10 minutes. Exiting...", topicName);
+            throw new RuntimeException("Topic '" + topicName + "' not found after waiting for 10 minutes.");
+        }
+    }
+
 }
